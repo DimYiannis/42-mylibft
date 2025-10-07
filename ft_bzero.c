febd1702c61caad4fb75bfc2ded5de79b9d68ee7
DimYiannis <johndim99@gmail.com>
2025-10-07 23:28:05 +0300

    bzero

---
+++ ft_bzero.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 23:13:03 by yiannis           #+#    #+#             */
+/*   Updated: 2025/10/07 23:27:02 by yiannis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void ft_bzero(void *s, size_t n)
+{
+    unsigned char *tmp; 
+    
+    tmp = (unsigned char *)s;
+    while (n > 0)
+    {
+        *tmp = '\0';
+        tmp++;
+        n--;
+    }
+}
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     char s[10] = "paok";
+//     int n = 2;
+//     char *p = s + n;
+//     ft_bzero(s, n);
+//     printf("%s", s);
+//     printf("%s", p);
+// }

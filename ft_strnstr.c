126dca3a2c2e4dd7a264618783173dd9c274fbc9
DimYiannis <johndim99@gmail.com>
2025-10-07 11:52:03 +0200

    missing norm

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 19:15:45 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/06 19:45:49 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/07 11:51:26 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ static int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (i < n && (s1[i] || s2[i]))
+	while (i < n && (s1[i] && s2[i]))
 	{
 		if (s1[i] == s2[i])
 		{
@@ -27,39 +27,43 @@ static int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 		}
 	}
 	return (0);
+
 }
 char	*ft_strnstr(const char *s1, const char *s2, unsigned int n)
 {
-    const char *haystack;
-    const char *needle;
+	const char	*haystack;
+	const char	*needle;
 
-    haystack = s1;
-    needle = s2;
-    
+	haystack = s1;
+	needle = s2;
 	while (*haystack && n > 0)
 	{
-		if (!needle)
+		if (*needle == '\0')
 		{
-			return ((char *) haystack);
+			return ((char *)haystack);
 		}
-        if (*haystack != *needle)
-        {
-            haystack++;
-            needle++;
-            n--;
-        }
+		if (*haystack != *needle)
+		{
+			haystack++;
+			n--;
+		}
 		else if (ft_strncmp(haystack, needle, n) == 0)
 		{
-            return ((char *) haystack);
+			return ((char *)haystack);
 		}
-		return (0);
+		else
+		{
+			haystack++;
+			n--;
+		}
 	}
+	return (0);
 }
 #include <stdio.h>
 
 int	main(void)
 {
 	char s1[10] = "aaaabaaa";
-	char s2[6] = "ab";
+	char s2[6] = "";
 	printf("%s", ft_strnstr(s1, s2, 6));
 }

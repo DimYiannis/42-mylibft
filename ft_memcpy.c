9797571ebe14924e6de155a00fc71a1c81968194
DimYiannis <johndim99@gmail.com>
2025-10-08 00:07:22 +0300

    memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 23:59:41 by yiannis           #+#    #+#             */
+/*   Updated: 2025/10/08 00:07:01 by yiannis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void *ft_memcpy(void *dst, const void *src, size_t n)
+{
+    unsigned char *pdst;
+    unsigned char *psrc;
+
+    pdst = (unsigned char *)dst;
+    psrc = (unsigned char *)src;
+}

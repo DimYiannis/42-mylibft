76e53f7afcda4aaa97e0632c7ee35a7279940c47
DimYiannis <johndim99@gmail.com>
2025-10-07 22:35:09 +0300

    works have to check norm

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 14:15:58 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/07 19:08:15 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/07 22:34:34 by yiannis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,35 +38,24 @@ int	ft_atoi(const char *str)
 	counter = 0;
 	sign = 0;
 	num = 0;
-	if (ft_isspace(*str) == 1 || *str == '+')
-	{
-		if (*str == '+')
-			counter++;
+	while (ft_isspace(*str) == 1)
 		str++;
-	}
-	while (*str)
+	while (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
-		{
 			sign = -1;
-			counter++;
-			str++;
-		}
-		else if (ft_isnum(*str) == 1)
-		{
-			num = *str - 48;
-			str++;
-			while (ft_isnum(*str) == 1)
-			{
-				num = num * 10 + (*str - 48);
-				str++;
-			}
-			if (counter > 1)
-				return (0);
-			return (num * sign);
-		}
+		counter++;
+		str++;
 	}
-	return (0);
+	while (ft_isnum(*str) == 1)
+	{
+		num = num * 10;
+		num = num + (*str - 48);
+		str++;
+	}
+	if (counter > 1)
+		return (0);
+	return (num * sign);		
 }
 
 #include <stdio.h>
@@ -76,7 +65,7 @@ int	main(void)
 {
 	int val;
 	char *str;
-	str = "+-15+09.10E";
+	str = "+---15+09.10E";
 	val = ft_atoi(str);
 	printf("integral number = %d \n", val);
 	val = atoi(str);

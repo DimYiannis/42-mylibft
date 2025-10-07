7fc9f35e94dcefea930df949735eba821ecd1a0f
DimYiannis <johndim99@gmail.com>
2025-10-07 22:54:47 +0300

    libft.h

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 22:46:53 by yiannis           #+#    #+#             */
+/*   Updated: 2025/10/07 22:48:40 by yiannis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H  
+# define FT_H
+
+int	ft_atoi(const char *str);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+
+#endif 

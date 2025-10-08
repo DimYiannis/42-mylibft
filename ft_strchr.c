26dca847ab6dc2c0c3f341dfc38fabed0890cafd
DimYiannis <johndim99@gmail.com>
2025-10-08 23:22:40 +0300

    strchr

---
+++ ft_strchr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 23:11:23 by yiannis           #+#    #+#             */
+/*   Updated: 2025/10/08 23:22:10 by yiannis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char *ft_strchr(const char *s, int c)
+{
+    while (*s)
+    {
+        if (*s == (char)c)
+            return ((char *)s);
+        s++;
+    }
+    if (c == '\0')
+        return ((char *)s);
+    return (NULL);
+}  
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     char s[10] = "paok";
+//     char c = 'a';
+//     printf("%s", ft_strchr(s, c));
+// }

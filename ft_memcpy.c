cb7822e57cbdd5db31e84878f0eff99882c8429b
DimYiannis <johndim99@gmail.com>
2025-10-08 22:07:14 +0300

    less lines

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 23:59:41 by yiannis           #+#    #+#             */
-/*   Updated: 2025/10/08 10:57:20 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/08 22:04:38 by yiannis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,12 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*pdst;
 	unsigned char	*psrc;
 
+    if (!dst && !src)
+		return (NULL);    
 	pdst = (unsigned char *)dst;
 	psrc = (unsigned char *)src;
-	while (n > 0)
-	{
+	while (n--)
 		*pdst++ = *psrc++;
-		n--;
-	}
 	return (dst);
 }
 

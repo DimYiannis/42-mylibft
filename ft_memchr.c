3d05122285baeaa993cc5b1be76b6fece089ccf5
DimYiannis <johndim99@gmail.com>
2025-10-08 23:52:13 +0300

    memchr

---
+++ ft_memchr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 23:36:04 by yiannis           #+#    #+#             */
+/*   Updated: 2025/10/08 23:51:41 by yiannis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    unsigned char *str;
+
+    str = (unsigned char *)s;
+    while (*str && n--)
+    {
+        if (*str == (char)c)
+            return ((void *)str);
+        str++;
+    }
+    if (c == '\0')
+        return ((char *)s);
+    return (NULL);
+}
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     char s[10] = "paok";
+//     char c = '\0';
+//     printf("%s", (char *)ft_memchr(s, c, 4));
+// }
+

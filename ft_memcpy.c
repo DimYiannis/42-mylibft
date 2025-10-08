221020ca08b130c0db5a580b65a1640b6d4cff7a
Yiannis Dimitrakopoulos <ydimitra@f1r4s4.codam.nl>
2025-10-08 10:57:44 +0200

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,9 +6,11 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 23:59:41 by yiannis           #+#    #+#             */
-/*   Updated: 2025/10/08 10:38:45 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/08 10:57:20 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stddef.h>
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
@@ -17,4 +19,20 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	pdst = (unsigned char *)dst;
 	psrc = (unsigned char *)src;
+	while (n > 0)
+	{
+		*pdst++ = *psrc++;
+		n--;
+	}
+	return (dst);
 }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     char dst[10] = "aris";
+//     char src[10] = "paok";
+//     ft_memcpy(dst, src, 3);
+//     printf("%s", dst);
+// }

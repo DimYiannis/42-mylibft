db6e47f69393fb486a5d513eb869b86fe4e29de2
DimYiannis <johndim99@gmail.com>
2025-10-09 23:26:24 +0300

    taking care of overflow

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 10:29:50 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/09 23:20:32 by yiannis          ###   ########.fr       */
+/*   Updated: 2025/10/09 23:26:07 by yiannis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void *calloc(size_t count, size_t size)
 	
     if (count == 0 || size == 0)
 		return (NULL);
-	if (size != 0 && count * size > SIZE_MAX)
+	if (size != 0 && count > SIZE_MAX / size)
 		return (NULL);
 	ptr = malloc(count * size);
 	if (!ptr)

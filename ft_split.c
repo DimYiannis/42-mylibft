a4a53ef749f8d1f9747da8b8f39e205f7f4b8385
DimYiannis <johndim99@gmail.com>
2025-10-10 22:41:36 +0300

    made split in less of 25 lines

--- ft_split.c
+++ ft_split.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 23:31:12 by yiannis           #+#    #+#             */
-/*   Updated: 2025/10/10 12:31:37 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/10 22:40:30 by yiannis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-static int	count_words(const char *s, char *c)
+static int	count_words(const char *s, char c)
 {
 	int	count;
 	int	in_word;
@@ -21,24 +21,24 @@ static int	count_words(const char *s, char *c)
 	in_word = 0;
 	while (*s)
 	{
-		if (*s != *c && !in_word)
+		if (*s != c && !in_word)
 		{
 			count++;
 			in_word = 1;
 		}
-		else if (*s == *c)
+		else if (*s == c)
 			in_word = 0;
 		s++;
 	}
 	return (count);
 }
 
-static int	word_length(const char *s, char *c)
+static int	word_length(char *s, char c)
 {
 	int	length;
 
 	length = 0;
-	while (*s && *s != *c)
+	while (*s && *s != c)
 	{
 		length++;
 		s++;
@@ -46,54 +46,51 @@ static int	word_length(const char *s, char *c)
 	return (length);
 }
 
-static void	free_up(char *s, int n)
+static void	*free_up(char **arr, int n)
 {
 	while (n-- > 0)
-		free(char s[n]);
-	free(s);
+		free(arr[n]);
+	free(arr);
 	return (NULL);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**eachword(char *s, char c , char **result)
 {
-	char	**result;
-	int		i;
-	int		len;
-	int		j;
-
-	if (!s)
-		return (NULL);
-	result = (char **)malloc(sizeof(char *) * (count_words(s, &c) + 1));
-	if (!result)
-		return (NULL);
+	int j;
+	int i;
+	
 	i = 0;
 	while (*s)
 	{
 		if (*s != c)
 		{
-			len = word_length(s, &c);
-			result[i] = (char *)malloc(sizeof(char) * (len + 1));
+			result[i] = malloc(word_length(s, c) + 1);
 			if (!result[i])
-			{
-                free_up(result[i], i);
-				// while (i-- > 0)
-				// 	free(result[i]);
-				// free(result);
-				// return (NULL);
-			}
+               free_up(result, i);
 			j = 0;
-			while (j < len)
-			{
-				result[i][j] = *s++;
-				j++;
-			}
-			result[i][len] = '\0';
+			while (*s && *s != c)
+				result[i][j++] = *s++;
+			result[i][j] = '\0';
 			i++;
 		}
 		else
 			s++;
 	}
+	
 	result[i] = NULL;
+	return (result);	
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**result;
+
+	if (!s)
+		return (NULL);
+	result = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!result)
+		return (NULL);
+	eachword((char *)s, c, result);
 	return (result);
 }
 

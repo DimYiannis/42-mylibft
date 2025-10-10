7caa43b3355ee0c0641a2c204317acb7e57a5618
DimYiannis <johndim99@gmail.com>
2025-10-10 23:49:17 +0300

    test cases for mem overflow

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 10:29:50 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/10 17:18:33 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/10 23:48:32 by yiannis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 static void	ft_bzero(void *s, size_t n)
 {
@@ -22,19 +23,19 @@ static void	ft_bzero(void *s, size_t n)
 		*tmp++ = '\0';
 }
 
-void	*calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
 	if (count == 0 || size == 0)
-		return (NULL);
+		return malloc(0);
 	if (size != 0 && count > SIZE_MAX / size)
 		return (NULL);
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
-	ft_bzero((void *)count, size);
-	return ((void *)count);
+	ft_bzero(ptr, count * size);
+	return ((void *)ptr);
 }
 
 // #include <stdio.h>
@@ -42,8 +43,42 @@ void	*calloc(size_t count, size_t size)
 // int main(void)
 // {
 // 	char s1[] = "paokara";
-// 	char s2[] = calloc((size_t)s1+4,sizeof(char) * 3);
+// 	char s2[] = ft_calloc(strlen(s1) + 1, sizeof(char));
 // 	printf("%s", s2);
 // 	free(s2);
 // 	return (0);
+// }
+
+// #include <stdint.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// int main(void)
+// {
+//     size_t count = SIZE_MAX;
+//     size_t size = 2;
+
+//     size_t total = count * size; 
+//     printf("Requested allocation: %zu bytes\n", count * size);
+//     printf("Wrapped-around total: %zu bytes\n", total);
+
+//     char *ptr = malloc(total);
+//     if (!ptr)
+//     {
+//         printf("Malloc failed\n");
+//         return 1;
+//     }
+
+//     size_t i;
+//     for (i = 0; i < 10; i++)
+//     {
+//         ptr[i] = 'A'; 
+//         printf("Writing ptr[%zu]\n", i);
+//     }
+
+//     printf("First byte: %c\n", ptr[0]);
+
+//     free(ptr);
+//     return 0;
 // }

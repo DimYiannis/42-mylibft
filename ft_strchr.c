135ac0ae4b3e6a1508193b663350b57adedb186e
Yiannis Dimitrakopoulos <ydimitra@f0r3s7.codam.nl>
2025-10-11 16:58:31 +0200

    array indx so that i wont lose the initial pointer

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 23:11:23 by yiannis           #+#    #+#             */
-/*   Updated: 2025/10/09 10:24:44 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/11 16:57:19 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,25 @@
 
 char	*ft_strchr(const char *s, int c)
 {
+	size_t i;
+
+	i = 0;
 	while (*s)
 	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
+		if (s[i] == (char)c)
+			return ((char *)s+i);
+		i++;
 	}
 	if (c == '\0')
 		return ((char *)s);
 	return (NULL);
 }
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main(void)
-// {
-//     char s[10] = "paok";
-//     char c = 'a';
-//     printf("%s", ft_strchr(s, c));
-// }
+int main(void)
+{
+    char s[10] = "paok";
+    char c = 'a';
+    printf("%s", ft_strchr(s, c));
+}

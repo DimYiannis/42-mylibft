9e9fab57415013630bf0a85b49d20ff668824bee
DimYiannis <johndim99@gmail.com>
2025-10-12 22:24:32 +0300

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 16:01:00 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/11 17:35:10 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/12 22:19:40 by yiannis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,24 @@
 #include <stdlib.h>
 #include "libft.h"
 
-static size_t	occurance(const char *s1, const char *set)
-{
-    size_t	lenset;
-	size_t	count;
-    size_t	i;
-    size_t j;
-
-    j = 0;
-    count = 0;
-    i = 0;
-    lenset = ft_strlen(set);
-	while (*s1)
-	{
-		while (i < lenset)
-		{
-			if (set[i] == s1[j])
-			{
-				count++;
-				i++;
-			}
-			i++;
-		}
-		i = 0;
-		j++;
-	}
-    return (count);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*s2;
-    size_t i;
+    size_t start;
+    size_t end;
 
-	if (!*s1)
-		return (NULL);
-	s2 = malloc((ft_strlen(s1) - occurance(s1, set)) + 1);
-    if (!*s2)
+    if (!s1 || !set)
         return (NULL);
-    i = 0;
-    while (*s1)
-    {
-        if (ft_strchr(set, *s1) != NULL)
-        {
-            s2[i] = *s1;
-            i++;
-        }   
-        s1++; 
-    }
+    start = 0;
+    end = ft_strlen(s1);
+    while (s1[start] && ft_strchr(set, s1[start]))
+        start++;
+    while (end > start && ft_strchr(set, s1[end - 1]))
+        end--;
+    s2 = malloc(end - start + 1);
+    if (!s2)
+        return (NULL);
+    ft_strlcpy(s2, s1 + start, end - start + 1);
     return (s2);
 }
 
@@ -71,6 +41,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 int main()
 {
     const char s[] = "abcdefghijklmnopqrstuvwxyz";
-    const char set[] = "ajlqz";
-    printf("%s", ft_strtrim(s, set));
+    const char set[] = "az";
+    char *res = ft_strtrim(s, set);
+    printf("%s", res);
+    free(res);
+    return (0);
 }

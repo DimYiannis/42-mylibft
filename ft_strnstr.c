1b8bf0ffe16dd6b36c04be7baf0a45aebb327d34
Yiannis Dimitrakopoulos <ydimitra@f1r2s16.codam.nl>
2025-10-13 10:33:12 +0200

    norm

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,27 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yiannis <yiannis@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 19:15:45 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/12 23:03:14 by yiannis          ###   ########.fr       */
+/*   Updated: 2025/10/13 10:32:44 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include "libft.h"
+#include <string.h>
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
 	const char	*haystack;
 	const char	*needle;
-	size_t needle_len;
+	size_t		needle_len;
 
 	haystack = s1;
 	needle = s2;
 	needle_len = ft_strlen(needle);
 	if (*needle == '\0')
-			return ((char *)haystack);
+		return ((char *)haystack);
 	while (*haystack && n > 0)
 	{
 		if (ft_strncmp(haystack, needle, needle_len) == 0)

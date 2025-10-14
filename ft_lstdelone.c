c39adf5810befe7b17de13dbcfa6ffb0ecfdfed6
DimYiannis <johndim99@gmail.com>
2025-10-14 23:29:12 +0300

    fix test using assert

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 17:49:03 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/14 20:08:30 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/14 23:28:48 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,50 @@ void ft_lstdelone(t_list *lst, void (*del)(void*))
     free(lst);
 }
 
-#include <stdio.h>
-#include  <assert.h>
+// #include <stdio.h>
+// #include  <assert.h>
 
-void del(void *content)
-{
-    free(content);
-}
+// void del(void *content)
+// {
+//     (void)content;
+// }
 
+// void	display_list(t_list *lst)
+// {
+// 	t_list	*tmp;
+//     if (lst == NULL)
+// 	{
+// 		printf("list is empty");
+// 	}
 
-int	main(void)
-{ 
-    t_list *lst = ft_lstnew((void*)0);
-    assert(lst->content == (void*)0);
-    assert(lst->next == NULL);
-    assert(ft_lstsize(lst) == 1);
-    ft_lstadd_back(&lst, ft_lstnew((void*)1));
-    assert(lst->content == (void*)0);
-    assert(lst->next->content == (void*)1);
-    assert(lst->next->next == NULL);
-    assert(ft_lstsize(lst) == 2);
-    ft_lstadd_back(&lst, ft_lstnew((void*)2));
-    assert(ft_lstsize(lst) == 3);
-    ft_lstadd_back(&lst, ft_lstnew((void*)3));
-    assert(ft_lstsize(lst) == 4);
-    ft_lstdelone(lst->next->next, del);
-    assert(ft_lstsize(lst) == 2);
-}
+//     tmp = lst;
+//     while (tmp != NULL)
+//     {
+//         printf("%p \n", tmp->content);
+// 			tmp = tmp->next;
+//     }
+
+// }
+
+// int	main(void)
+// { 
+//     t_list *lst = ft_lstnew((void*)0);
+//     assert(lst->next == NULL);
+//     assert(ft_lstsize(lst) == 1);
+    
+//     ft_lstadd_back(&lst, ft_lstnew((void*)1));
+//     ft_lstadd_back(&lst, ft_lstnew((void*)2));
+//     ft_lstadd_back(&lst, ft_lstnew((void*)3));
+//     assert(ft_lstsize(lst) == 4);
+    
+//     display_list(lst);
+    
+//     t_list *tmp = lst->next->next; //move 2 nodes
+//     lst->next->next = tmp->next; // unlink from list
+//     ft_lstdelone(tmp, del); //del
+    
+//     assert(ft_lstsize(lst) == 3);
+//     printf("\n");
+//     display_list(lst);
+    
+// }

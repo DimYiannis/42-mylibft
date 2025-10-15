99b288ffdf8a74a323b229e1c493a326f76563a1
Yiannis Dimitrakopoulos <ydimitra@f1r1s19.codam.nl>
2025-10-15 18:03:33 +0200

    norm

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 11:26:20 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/15 17:56:18 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/15 18:02:28 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,54 +40,52 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	return (head);
 }
 
-#include <assert.h>
-#include <stdio.h>
+// #include <assert.h>
+// #include <stdio.h>
 
-void	*f(void *content)
-{
-	int	*newdata;
+// void	*f(void *content)
+// {
+// 	int	*newdata;
 
-	newdata = content;
-	newdata = malloc(sizeof(int));
-	if (!newdata)
-		return (NULL);
-	*newdata = 99;
-	return (newdata);
-}
+// 	newdata = content;
+// 	newdata = malloc(sizeof(int));
+// 	if (!newdata)
+// 		return (NULL);
+// 	*newdata = 99;
+// 	return (newdata);
+// }
 
-void	del(void *content)
-{
-	free(content);
-}
+// void	del(void *content)
+// {
+// 	free(content);
+// }
 
-int	main(void)
-{
-	int	i;
-	int	*val;
+// int	main(void)
+// {
+// 	int		i;
+// 	int		*val;
+// 	t_list	*lst1;
+// 	t_list	*lst2;
 
-	t_list *lst1 = NULL;
-	t_list *lst2;
-
-	i = 0;
-	while (i < 4)
-	{
-		val = malloc(sizeof(int));
-		if (!val)
-			return (1);
-		*val = i;
-		ft_lstadd_back(&lst1, ft_lstnew(val));
-		i++;
-	}
-	assert(ft_lstsize(lst1) == 4);
-	
-	lst2 = ft_lstmap(lst1, &f, del);
-	assert(ft_lstsize(lst2) == 4);
-	
-	while (lst2)
-	{
-		assert(*(int *)lst2->content == 99);
-		lst2 = lst2->next;
-	}
-	printf("bellissima");
-	return (0);
-}
+// 	lst1 = NULL;
+// 	i = 0;
+// 	while (i < 4)
+// 	{
+// 		val = malloc(sizeof(int));
+// 		if (!val)
+// 			return (1);
+// 		*val = i;
+// 		ft_lstadd_back(&lst1, ft_lstnew(val));
+// 		i++;
+// 	}
+// 	assert(ft_lstsize(lst1) == 4);
+// 	lst2 = ft_lstmap(lst1, &f, del);
+// 	assert(ft_lstsize(lst2) == 4);
+// 	while (lst2)
+// 	{
+// 		assert(*(int *)lst2->content == 99);
+// 		lst2 = lst2->next;
+// 	}
+// 	printf("bellissima");
+// 	return (0);
+// }

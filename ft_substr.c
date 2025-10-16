03a3e8f3e1469c1a748895fee0e605d97a58981e
Yiannis Dimitrakopoulos <ydimitra@f1r1s16.codam.nl>
2025-10-16 16:50:21 +0200

    need to fix thissss

--- ft_substr.c
+++ ft_substr.c
@@ -6,25 +6,32 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 23:06:50 by yiannis           #+#    #+#             */
-/*   Updated: 2025/10/11 15:55:17 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/16 16:45:49 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stddef.h>
 #include <stdlib.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*string;
-	size_t	i;
+	char			*string;
+	size_t			i;
+	unsigned int	slen;
 
-	i = 0;
-	if (!*s)
+	slen = ft_strlen(s);
+	if (!s)
 		return (NULL);
+	if (start >= slen)
+		return (ft_strdup(""));
+	if (len > slen -start)
+		len = slen - start;
 	s = s + start;
 	string = malloc(len + 1);
 	if (!string)
 		return (NULL);
+	i = 0;
 	while (i < len)
 	{
 		string[i] = *s;
@@ -35,10 +42,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (string);
 }
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main()
-// {
-//    const char s[] = "abcdefghijklmnopqrstuvwxyz";
-//     printf("%s", ft_substr(s, 5, 5));
-// }
+int main()
+{
+   const char s[] = "abcdefg";
+    printf("%s", ft_substr(s, 15, 5));
+}

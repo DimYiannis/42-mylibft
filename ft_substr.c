a155cc56ed9393d5032454efd6be23eebefed080
DimYiannis <johndim99@gmail.com>
2025-10-16 20:38:52 +0300

    calc len after checking

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 23:06:50 by yiannis           #+#    #+#             */
-/*   Updated: 2025/10/16 16:45:49 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/16 20:37:47 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,32 +20,33 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t			i;
 	unsigned int	slen;
 
-	slen = ft_strlen(s);
 	if (!s)
 		return (NULL);
+	slen = ft_strlen(s);
 	if (start >= slen)
 		return (ft_strdup(""));
 	if (len > slen -start)
 		len = slen - start;
-	s = s + start;
 	string = malloc(len + 1);
 	if (!string)
 		return (NULL);
 	i = 0;
 	while (i < len)
 	{
-		string[i] = *s;
-		s++;
+		string[i] = s[start + i];
 		i++;
 	}
 	string[i] = '\0';
 	return (string);
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-   const char s[] = "abcdefg";
-    printf("%s", ft_substr(s, 15, 5));
-}
+// int main()
+// {
+//    const char s[] = "abcdefg";
+//     printf("0: %s\n", ft_substr(s, 15, 5));
+// 	printf("1: %s\n", ft_substr(s, 2, 3));
+// 	printf("2: %s\n", ft_substr(s, 0, 20));
+
+// }
